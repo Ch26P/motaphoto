6f1cb864527df1d8a52ee9e11d1543b8092f996e
Pochon Christophe <chris26-78@hotmail.fr>
2023-12-18 00:31:09 +0100

    divers modif modal contact

--- js/modal_contact.js
+++ js/modal_contact.js
@@ -1,25 +1,41 @@
 //Modal Contact
 jQuery(document).ready(function ($) {
-    //Le code ici
+  //  console.log($(referenceValue));
+    console.log( $("#ref"));
     console.log($(' #menu-navigation li:last '));
-    console.log($('#modale_contact'));
+    console.log($('.modale_contact'));
     $('nav .lien_contact').click(function () {
-   //  $('#modale_contact').toggle();
-        $('#modale_contact').slideToggle();
+        $('.modale_contact').slideToggle();
+
     });
-
-
 });
+//**ajout valeur ref formulaire contact */
 
+  jQuery(document).ready(function($){
+    var referenceValue = $( "#description ul li:first span" ).html();
+  //  console.log(referenceValue);
+    $("#ref").val(referenceValue);
+  });
+
+//fermeture modal contact
+jQuery(document).ready(function ($) {       //A Finir
+                                             //controler la fermeture 
+ 
+    //   console.log($(' .modale_contact_bloc '));
+    console.log($('.modale_contact_bloc'));//
+    $('.modale_contact_bloc').click(function () {
+        $('.modale_contact').slideToggle();
+    });
+});
 //Modal menu mobile
 
 jQuery(document).ready(function ($) {
     //Le code ici
     console.log($(' button.mobile_menu '));
     console.log($('.menu-navigation-container'));
-   $('button.mobile_menu').click(function () {
-   //  $('#modale_contact').toggle();
+    $('button.mobile_menu').click(function () {
+        //  $('#modale_contact').toggle();
         $('.burger_menu').slideToggle();
         $(' button.mobile_menu').toggleClass('toggled')
-    });/* */
+    });
 });
--- template-parts/modal-contact.php
+++ template-parts/modal-contact.php
@@ -1,5 +1,11 @@
-<div id="modale_contact">
-    <div class="contact">
-        <?php echo do_shortcode(' [contact-form-7 id="be37a03" title="Formulaire de contact 1"]') ?>
+<div id="Modale_contact" class="modale_contact">
+    <div class="modale_contact_bloc">
+        <div class="modale_contact_content">
+            <div class="modale_contact_content_bloc_title">
+            <div class="modale_contact_content_title"></div>
+            <div class="modale_contact_content_title"></div>
+            </div>
+            <?php echo do_shortcode(' [contact-form-7 id="be37a03" title="Formulaire de contact 1"]') ?>
+        </div>
     </div>
 </div>
